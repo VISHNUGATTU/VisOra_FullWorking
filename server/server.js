f2ae189a157ef6fa1734e8742240267430aaa56d
VISHNUGATTU <gattuvishnuphani@gmail.com>
2026-02-20 20:11:49 +0530

    20Feb2k26-evng2

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 // Initialize app
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 // Connect Database & Cloudinary
