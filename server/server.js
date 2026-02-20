f04522b8898f19655627b8bed67ddc654f51a152
VISHNUGATTU <gattuvishnuphani@gmail.com>
2026-02-20 19:31:22 +0530

    20Feb2k26-mrng

--- server/server.js
+++ server/server.js
@@ -30,30 +30,22 @@ await connectDB();
 connectCloudinary();
 
 // ✅ Allowed Origins (Local + Production)
+
 const allowedOrigins = [
   "http://localhost:5173",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true
+  })
+);
 // ✅ Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
 
 // ✅ Test Route
 app.get("/", (req, res) => {
